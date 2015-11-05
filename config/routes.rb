41bf654177b12cda3ac3a55137a4684e0d073b39
Matt Stjernholm <thew88@gmail.com>
2015-11-05 15:15:12 -0700

    Implement user log in and log out test
    * basic tests complete - still need to do authorization

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show, :index]
 
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
 end
--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -14,5 +14,39 @@ class UserLoginTest < ActionDispatch::IntegrationTest
 
     assert page.has_content?("Welcome, MyName!")
   end
-  
+
+  test "a registered user cannot submit an incorrect password" do
+    User.create(username: "MyName", password: "MyPassword")
+
+    visit login_path
+
+    fill_in "Username", with: "MyName"
+    fill_in "Password", with: "MyPasswordiswrong"
+    click_button "Login"
+
+    assert page.has_content?("Nope....that's not right. Try again.")
+  end
+
+  test "an unregistered user cannot log in" do
+    visit login_path
+
+    fill_in "Username", with: "MyNamedoesntexist"
+    fill_in "Password", with: "MyPasswordiswrong"
+    click_button "Login"
+
+    assert page.has_content?("Nope....that's not right. Try again.")
+  end
+
+
+    test "a registered user can log out" do
+      login_user
+
+      assert page.has_content?("Welcome, MyName!")
+
+      click_link "Logout"
+
+      assert_equal login_path, current_path
+      assert page.has_content?("You've been logged out")
+    end
+
 end
